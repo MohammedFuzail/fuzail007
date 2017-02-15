0e448291580c9916f65948c9c8eaa4c2ff656553
Fuzail <mohammaddfuzail@gmail.com>
2017-02-15 14:32:24 +0530

    mysitee

--- myonlinestore/temp/storescripts/mysql_quicktest.php
+++
@@ -1,7 +0,0 @@
-<?php
-//connect to the file above here
-require "connect_to_mysql.php";
-
-echo "<h1>Success in database connection! Happy Coding!</h1>";
-
-?>
--- myonlinestore/template_footer.php
+++
@@ -1,3 +0,0 @@
-<div id = "pageFooter">Copyright
-|<a href="http://localhost/myonlinestore/storeadmin/admin_login.php">Admin</a>
-</div>
